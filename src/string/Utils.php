574e92f6a761426081628f23c4a51911a9406e99
poniyur <94poniyur@mail.ru>
2020-05-29 14:38:04 +1000

    Initial commit

---
+++ src/string/Utils.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace string;
+
+class Utils {
+
+	public static function validateBrakets($str) {
+		$str = preg_replace('/\s*/', '', $str);
+		$bracketCounter = 0;
+		for($i = 0, $iEnd = strlen($str); $i < $iEnd; $i++) {
+
+			$char = $str[$i];
+			switch ($char) {
+				case '(':
+					$bracketCounter++;
+					break;
+
+				case ')':
+					$bracketCounter--;
+					if( $bracketCounter < 0 ) return false;
+					break;
+
+				default:
+					throw new InvalidArgumentException("Only brackets allowed! You use char '$char'");
+					break;
+			}
+		}
+
+		if( $bracketCounter === 0 ) {
+			return true;
+		} else {
+			return false;
+		}
+
+	}
+
+}
